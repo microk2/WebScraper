b664de9f54247a7e93387f823c82e3cf6226dcd8
micro <tdanalache@gmail.com>
2023-01-22 19:32:30 +0100

    Delete useless stuff from main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,1 @@
-import Database from "./classes/Database";
-
-(async () => {
-  const db = new Database();
-  await db.connect();
-  console.log(await db.getQuestline([1314, 1315, 1316, 1317]));
-
-  process.exit(0);
-})();
+(async () => {})();
